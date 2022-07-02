df81c9c8c051d15ba857cc33a1e79981db93021e
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-07-02 12:48:59 +0100

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -4,8 +4,8 @@ from pytube import YouTube
 st.title('YouTube Video Downloader')
 
 youtube_url = st.text_input('Please paste the URL for your YouTube Video')
-'''
-if youtube_url is not None:
+
+if youtube_url:
     yt = YouTube(youtube_url)
     with st.spinner('Processing....please wait'):
         downloaded_video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
@@ -21,4 +21,3 @@ if youtube_url is not None:
                  mime="video/mp4"
                )
     st.image(yt.thumbnail_url)
-'''
