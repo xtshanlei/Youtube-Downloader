ba11b589dce6f429d8310d12a2aaa09c1d8a5dd3
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-07-02 11:18:33 +0100

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -5,4 +5,6 @@ st.title('YouTube Video Downloader')
 
 youtube_url = st.text_input('Please paste the URL for your YouTube Video')
 yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
-yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+downloaded_video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+
+st.video(downloaded_video)
