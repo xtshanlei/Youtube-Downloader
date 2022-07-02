9e5c4c2b24210094020519f0ed5df56f0e9aae71
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-07-02 11:26:05 +0100

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -8,4 +8,9 @@ yt = YouTube(youtube_url)
 downloaded_video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
 st.write(yt.title)
+st.download_button(
+     label="Download video",
+     data=downloaded_video,
+     file_name='{}.csv'.format(yt.title)
+ )
 st.video(downloaded_video)
