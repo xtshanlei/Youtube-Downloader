e1e202c6387f6b481f1939bb33c445c52170b6c2
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-07-05 12:38:19 +0100

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -13,7 +13,7 @@ if youtube_url:
         caption_language= yt.captions
         downloaded_caption = yt.captions.get_by_language_code('zh-TW').xml_captions
     st.success("Done! Click the 'Download video' button to download your video!")
-
+    st.write(type(downloaded_caption))
 
     st.header(yt.title)
     with open(downloaded_video, "rb") as video:
