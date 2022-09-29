82165b992c6fe4ffcf1603e85f25fe66de26dd17
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-09-29 08:34:41 +0100

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -6,7 +6,7 @@ st.write('----by Yulei')
 
 youtube_url = st.text_input('Please paste the URL for your YouTube Video')
 
-@st.cache
+
 def extract_video(yt):
     downloaded_video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
     caption_language= yt.captions
